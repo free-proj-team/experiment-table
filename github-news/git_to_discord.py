3d815b76434df8c0ada1b771dd01727ab83f9cb3
Hephai Lee <icetime963@gmail.com>
2022-12-18 21:33:07 +0900

    Fix: bug

--- github-news/git_to_discord.py
+++ github-news/git_to_discord.py
@@ -7,11 +7,12 @@ DISCORD_HOOK_URL = os.environ.get("DISCORD_HOOK_URL")
 
 @router.post("/send", status_code=status.HTTP_204_NO_CONTENT, response_model=None)
 def send(raw_payload: dict = Body(), event: str = Header(default=..., alias="X-GitHub-Event")):
-    content = f"{event.upper} 했어요! -{raw_payload['sender']['login']}-\n레포지토리 : {raw_payload['name']}\n"
+    content = f"{event.upper()} 했어요! -{raw_payload['sender']['login']}-\n레포지토리 : {raw_payload['name']}\n"
     if "compare" in raw_payload:
         content += f"변경점 확인 : {raw_payload['compare']}"
 
-    post(url=DISCORD_HOOK_URL, data={ "content": content }, headers={ "Content-Type": "application/json" })
+    res = post(url=DISCORD_HOOK_URL, data={ "content": content })
+    print(res)
 
 @router.post("/echo", status_code=status.HTTP_200_OK, response_model=None)
 def echo(body: dict = Body()):
